789c45bee67c8020dc5c294fd0bf7d22cbb8578c
kinoubenkyou <kinoubenkyou@gmail.com>
2023-12-15 00:00:49 +0700

    Create 6_2_max_product_of_three.py

---
+++ 6_2_max_product_of_three.py
@@ -0,0 +1,8 @@
+def solution(A):
+    possible_results = set()
+    if 0 in A:
+        possible_results.add(0)
+    sorted_ = sorted(A)
+    possible_results.add(sorted_[0] * sorted_[1] * sorted_[-1])
+    possible_results.add(sorted_[-3] * sorted_[-2] * sorted_[-1])
+    return max(possible_results)
