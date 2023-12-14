26cb7c037557b12f4f0fff75795adc909fc81a70
kinoubenkyou <kinoubenkyou@gmail.com>
2023-12-14 23:01:40 +0700

    Update 4_4_missing_integer.py

--- 4_4_missing_integer.py
+++ 4_4_missing_integer.py
@@ -1,6 +1,6 @@
 def solution(A):
     set_ = set([n for n in A if n > 0])
-    result = 1
-    while result in set_:
-        result += 1
-    return result
+    missing = 1
+    while missing in set_:
+        missing += 1
+    return missing
