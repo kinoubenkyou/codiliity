5e1ce2691a8cea05874f42b6d932431b665aebf2
kinoubenkyou <kinoubenkyou@gmail.com>
2023-12-14 22:41:35 +0700

    Update 1_1_binary_gap.py

--- 1_1_binary_gap.py
+++ 1_1_binary_gap.py
@@ -1,9 +1,9 @@
 def solution(N):
     string = bin(N)[2:]
-    result = 0
+    gap_length = 0
     last_start = 0
     for i in range(1, len(string)):
         if string[i] == "1":
-            result = max(result, i - last_start - 1)
+            gap_length = max(result, i - last_start - 1)
             last_start = i
-    return result
+    return gap_length
