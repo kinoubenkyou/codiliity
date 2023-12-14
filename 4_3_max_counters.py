dd70219881773e0f9490bd4734e065082244708b
kinoubenkyou <kinoubenkyou@gmail.com>
2023-12-14 23:01:12 +0700

    Update 4_3_max_counters.py

--- 4_3_max_counters.py
+++ 4_3_max_counters.py
@@ -1,10 +1,10 @@
 def solution(N, A):
     maxed_to = 0
     increase_dict = {}
-    for n in A:
-        if n == N + 1:
+    for number in A:
+        if number == N + 1:
             maxed_to += max(increase_dict.values() or [0])
             increase_dict = {}
         else:
-            increase_dict[n] = increase_dict.get(n, 0) + 1
-    return [maxed_to + increase_dict.get(n + 1, 0) for n in range(N)]
+            increase_dict[n] = increase_dict.get(number, 0) + 1
+    return [maxed_to + increase_dict.get(n + 1, 0) for number in range(N)]
