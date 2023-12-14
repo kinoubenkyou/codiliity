14d379b6c0cbbf29ca8a04dd30a2c1a484f4abc3
kinoubenkyou <kinoubenkyou@gmail.com>
2023-12-14 22:59:27 +0700

    Update 4_2_perm_check.py

--- 4_2_perm_check.py
+++ 4_2_perm_check.py
@@ -1,11 +1,11 @@
 def solution(A):
     set_ = set()
-    for n in A:
-        if n in set_:
+    for number in A:
+        if number in set_:
             return 0
         else:
-            set_.add(n)
-    for n in range(1, max(A) + 1):
-        if n not in set_:
+            set_.add(number)
+    for number in range(1, max(A) + 1):
+        if number not in set_:
             return 0
     return 1
