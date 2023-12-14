6200740521502a462a2fdf6627430c7cb349ec36
kinoubenkyou <kinoubenkyou@gmail.com>
2023-12-14 22:56:06 +0700

    Update 3_2_perm_missing_elem.py

--- 3_2_perm_missing_elem.py
+++ 3_2_perm_missing_elem.py
@@ -2,6 +2,8 @@ def solution(A):
     if not A:
         return 1
     max_ = max(A)
-    if max_ == len(A):
-        return max_ + 1
-    return (set(range(1, max_ + 1)) - set(A)).pop()
+    set_ = set(A)
+    for number in range(1, max_ + 1):
+        if number not in set_:
+            return number
+    return max_ + 1
