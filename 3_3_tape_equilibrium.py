2d31ccce35160aaf10f86b62ff0d79d2f625eeb0
kinoubenkyou <kinoubenkyou@gmail.com>
2023-12-14 22:49:12 +0700

    Update 3_3_tape_equilibrium.py

--- 3_3_tape_equilibrium.py
+++ 3_3_tape_equilibrium.py
@@ -1,9 +1,9 @@
 def solution(A):
     left_sum = A[0]
     right_sum = sum(A[1:])
-    result = abs(left_sum - right_sum)
+    difference = abs(left_sum - right_sum)
     for i in range(2, len(A)):
         left_sum += A[i - 1]
         right_sum -= A[i - 1]
-        result = min(result, abs(left_sum - right_sum))
-    return result
+        difference = min(difference, abs(left_sum - right_sum))
+    return difference
