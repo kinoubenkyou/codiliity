5cb8f898a86d1d4034aeba306b953f3cf6683d5a
kinoubenkyou <kinoubenkyou@gmail.com>
2023-12-14 20:53:25 +0700

    Create odd_occurrences_in_array.py

---
+++ odd_occurrences_in_array.py
@@ -0,0 +1,9 @@
+def solution(A):
+    set_ = set()
+    for number in A:
+        if number in set_:
+            set_.remove(number)
+        else:
+            set_.add(number)
+    return set_.pop()
+  
