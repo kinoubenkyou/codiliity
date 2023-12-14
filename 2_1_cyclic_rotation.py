be7a756963ac4853a1fcfe54ff699ea3632a670d
kinoubenkyou <kinoubenkyou@gmail.com>
2023-12-14 20:57:06 +0700

    Update 2_1_cyclic_rotation.py

--- 2_1_cyclic_rotation.py
+++ 2_1_cyclic_rotation.py
@@ -1,5 +1,5 @@
 def solution(A, K):
     if not A:
         return A
-    rotate_times = K % len(A)
-    return A[-rotate_times:] + A[:-rotate_times]
+    rotates = K % len(A)
+    return A[-rotates:] + A[:-rotates]
